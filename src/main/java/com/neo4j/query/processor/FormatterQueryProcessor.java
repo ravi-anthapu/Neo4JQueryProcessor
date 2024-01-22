92273b9f51055e2f37a331a1afda4832a66e8a5d
Ravindranatha Anthapu <ravi.anthapu@neo4j.com>
2024-01-22 17:00:57 -0500

    Fixed few small issues with parsing formatted data.

--- src/main/java/com/neo4j/query/processor/FormatterQueryProcessor.java
+++ src/main/java/com/neo4j/query/processor/FormatterQueryProcessor.java
@@ -80,119 +80,120 @@ public class FormatterQueryProcessor implements QueryProcessor {
 
     private QueryRecord readQueryLogEntry(String query) {
         QueryRecord record = new QueryRecord();
-        String[] splitData = query.split(" - ") ;
-        record.timeStamp = Timestamp.valueOf(splitData[0].substring(0, 19).replace('T', ' '));
-        String s = null ;
+        String[] splitData = query.split(" - ");
+        String s = null;
+        int curPart = 0;
 
-        int curPart = 0 ;
+        try {
+            record.timeStamp = Timestamp.valueOf(splitData[0].substring(0, 19).replace('T', ' '));
 
-        String current = splitData[curPart] ;
-        if( current.contains("Query started:") ) {
-            record.isStartRecord = true ;
-        } else {
-            record.isStartRecord = false ;
-        }
-        int nextIndex = -1 ;
-        int index = current.indexOf("transaction id:") ;
-        if( index > 0 ) {
-            // First part contais the Transaciton ID. This means we might not have query id.
-            s = getKeyValue(current, "transaction id:", ' ') ;
-            if( s != null ) {
-                record.dbTransactionId = Long.valueOf(s.trim()) ;
+            String current = splitData[curPart];
+            if (current.contains("Query started:")) {
+                record.isStartRecord = true;
             } else {
-                record.dbTransactionId = -1 ;
+                record.isStartRecord = false;
             }
-        } else {
-            // First part foes not contain Transaction id.
-            s = getKeyValue(current, "id:", ' ') ;
-            if( s != null ) {
-                record.dbQueryId = Long.valueOf(s.trim()) ;
-            } else {
-                record.dbQueryId = -1 ;
-            }
-        }
-        curPart++ ;
-        current = splitData[curPart] ;
-        if( current.contains("transaction id:")) {
-            s = getKeyValue(current, "transaction id:", ' ') ;
-            if( s != null ) {
-                record.dbTransactionId = Long.valueOf(s.trim()) ;
-            } else {
-                record.dbTransactionId = -1 ;
-            }
-            curPart++ ;
-            current = splitData[curPart] ;
-        }
-        current = current.trim() ;
-        index = current.indexOf(' ') ;
-        if( index > 0 ) {
-            s = current.substring(0,index).trim() ;
-            record.elapsedTimeMs = Long.valueOf(s) ;
-            if( record.elapsedTimeMs == 0 ) {
-                record.elapsedTimeMs = 1 ;
-            }
-        }
-
-        s = getKeyValue(current, "planning:", ',') ;
-        if( s != null ) {
-            record.planning = Long.valueOf(s.trim()) ;
-        }
-        s = getKeyValue(current, "cpu:", ' ') ;
-        if( s != null ) {
-            record.cpuTime = Long.valueOf(s.trim()) ;
-        } else {
-            s = getKeyValue(current, "cpu:", ')') ;
-            if( s != null ) {
-                record.cpuTime = Long.valueOf(s.trim()) ;
-            }
-        }
-        s = getKeyValue(current, "waiting:", ')') ;
-        if( s != null ) {
-            record.waiting = Long.valueOf(s.trim()) ;
-        }
-
-        // Process Bytes
-        curPart++ ;
-        current = splitData[curPart].trim() ;
-        index = current.indexOf(' ') ;
-        record.allocatedBytes = Long.valueOf(current.substring(0,index)) ;
-
-        // Process Page Hits and Page Faults
-        curPart++ ;
-        current = splitData[curPart].trim() ;
-        index = current.indexOf(' ') ;
-        record.pageHits = Long.valueOf(current.substring(0,index)) ;
-        index = current.indexOf(',') ;
-        nextIndex = current.indexOf(' ', index+2) ;
-        s = current.substring(index+2,nextIndex) ;
-        record.pageFaults = Long.valueOf(s) ;
-
-        // Process Source Data
-        curPart++ ;
-        current = splitData[curPart].trim() ;
-
-        if( current.startsWith("embedded-session")) {
-            record.client = "embedded-session" ;
-            record.authenticatedUser = current.substring(16).trim() ;
-            record.executedUser = record.authenticatedUser;
-        } else {
-            StringTokenizer tokens = new StringTokenizer(current, "\\\t") ;
-            String session = tokens.nextToken() ;
-            if( session.equals("bolt-session")) {
-                tokens.nextToken() ;
-
-                String driverTxt = tokens.nextToken() ;
-                int iindex = driverTxt.indexOf("/") ;
-                if( iindex > 0 ) {
-                    record.driver = driverTxt.substring(0,iindex) ;
-                    record.driverVersion = driverTxt.substring(iindex+1) ;
+            int nextIndex = -1;
+            int index = current.indexOf("transaction id:");
+            if (index > 0) {
+                // First part contais the Transaciton ID. This means we might not have query id.
+                s = getKeyValue(current, "transaction id:", ' ');
+                if (s != null) {
+                    record.dbTransactionId = Long.valueOf(s.trim());
+                } else {
+                    record.dbTransactionId = -1;
                 }
-                record.client = getKeyValue(tokens.nextToken(), "client/", ':') ;
-                record.server = getKeyValue(tokens.nextToken(), "server/", ':') ;
             } else {
-                System.out.println("Unknown Session : " + current);
+                // First part foes not contain Transaction id.
+                s = getKeyValue(current, "id:", ' ');
+                if (s != null && !s.trim().equals("")) {
+                    record.dbQueryId = Long.valueOf(s.trim());
+                } else {
+                    record.dbQueryId = -1;
+                }
             }
-        }
+            curPart++;
+            current = splitData[curPart];
+            if (current.contains("transaction id:")) {
+                s = getKeyValue(current, "transaction id:", ' ');
+                if (s != null) {
+                    record.dbTransactionId = Long.valueOf(s.trim());
+                } else {
+                    record.dbTransactionId = -1;
+                }
+                curPart++;
+                current = splitData[curPart];
+            }
+            current = current.trim();
+            index = current.indexOf(' ');
+            if (index > 0) {
+                s = current.substring(0, index).trim();
+                record.elapsedTimeMs = Long.valueOf(s);
+                if (record.elapsedTimeMs == 0) {
+                    record.elapsedTimeMs = 1;
+                }
+            }
+
+            s = getKeyValue(current, "planning:", ',');
+            if (s != null) {
+                record.planning = Long.valueOf(s.trim());
+            }
+            s = getKeyValue(current, "cpu:", ',');
+            if (s != null) {
+                record.cpuTime = Long.valueOf(s.trim());
+            } else {
+                s = getKeyValue(current, "cpu:", ')');
+                if (s != null) {
+                    record.cpuTime = Long.valueOf(s.trim());
+                }
+            }
+            s = getKeyValue(current, "waiting:", ')');
+            if (s != null) {
+                record.waiting = Long.valueOf(s.trim());
+            }
+
+            // Process Bytes
+            curPart++;
+            current = splitData[curPart].trim();
+            index = current.indexOf(' ');
+            record.allocatedBytes = Long.valueOf(current.substring(0, index));
+
+            // Process Page Hits and Page Faults
+            curPart++;
+            current = splitData[curPart].trim();
+            index = current.indexOf(' ');
+            record.pageHits = Long.valueOf(current.substring(0, index));
+            index = current.indexOf(',');
+            nextIndex = current.indexOf(' ', index + 2);
+            s = current.substring(index + 2, nextIndex);
+            record.pageFaults = Long.valueOf(s);
+
+            // Process Source Data
+            curPart++;
+            current = splitData[curPart].trim();
+
+            if (current.startsWith("embedded-session")) {
+                record.client = "embedded-session";
+                record.authenticatedUser = current.substring(16).trim();
+                record.executedUser = record.authenticatedUser;
+            } else {
+                StringTokenizer tokens = new StringTokenizer(current, "\\\t");
+                String session = tokens.nextToken();
+                if (session.equals("bolt-session")) {
+                    tokens.nextToken();
+
+                    String driverTxt = tokens.nextToken();
+                    int iindex = driverTxt.indexOf("/");
+                    if (iindex > 0) {
+                        record.driver = driverTxt.substring(0, iindex);
+                        record.driverVersion = driverTxt.substring(iindex + 1);
+                    }
+                    record.client = getKeyValue(tokens.nextToken(), "client/", ':');
+                    record.server = getKeyValue(tokens.nextToken(), "server/", ':');
+                } else {
+                    System.out.println("Unknown Session : " + current);
+                }
+            }
 //        if( current.contains("bolt-session")) {
 //            // Bolt Sesstion.
 //            record.server = getKeyValue(current, "server/", ':') ;
@@ -206,47 +207,51 @@ public class FormatterQueryProcessor implements QueryProcessor {
 //            }
 //        }
 
-        curPart++ ;
-        if( record.client!= null && !record.client.equals("embedded-session")) {
-            record.authenticatedUser = splitData[curPart] ;
-            record.executedUser = splitData[curPart] ;
-        }
-
-        curPart++ ;
-        // Process the runtime and skip metadata and parameters
-        // Also set the last part location.
-        int len = splitData.length ;
-        int lastPart ;
-        s = splitData[len-2].trim() ;
-        if( s.startsWith("runtime=")) {
-            record.runtime = s.substring(8).trim() ;
-            if( record.runtime != null && record.runtime.equals("null")) {
-                record.runtime = null ;
+            curPart++;
+            if (record.client != null && !record.client.equals("embedded-session")) {
+                record.authenticatedUser = splitData[curPart];
+                record.executedUser = splitData[curPart];
             }
-            lastPart = len - 2 ;
-        } else if(splitData[len-3].trim().startsWith("runtime=")){
-            // We have error at the end.
-            record.stackTrace = splitData[len-1].trim() ;
-            record.failed = 1 ;
-            lastPart = len - 3 ;
-        } else {
-            lastPart = len - 1 ;
-        }
-        // Skip any parameters that have the same pattern as " - "
-        while (true) {
-            if( splitData[lastPart-1].startsWith("{")) {
-                break;
-            }
-            lastPart-- ;
-        }
-        // Get the Query
-        StringBuffer queryStringBuffer = new StringBuffer() ;
-        for( int i = curPart ; i < lastPart-1 ; i++ ) {
-            queryStringBuffer.append(splitData[i]) ;
-        }
-        record.query = queryStringBuffer.toString().trim() ;
 
-        return record ;
+            curPart++;
+            // Process the runtime and skip metadata and parameters
+            // Also set the last part location.
+            int len = splitData.length;
+            int lastPart;
+            s = splitData[len - 2].trim();
+            if (s.startsWith("runtime=")) {
+                record.runtime = s.substring(8).trim();
+                if (record.runtime != null && record.runtime.equals("null")) {
+                    record.runtime = null;
+                }
+                lastPart = len - 2;
+            } else if (splitData[len - 3].trim().startsWith("runtime=")) {
+                // We have error at the end.
+                record.stackTrace = splitData[len - 1].trim();
+                record.failed = 1;
+                lastPart = len - 3;
+            } else {
+                lastPart = len - 1;
+            }
+            // Skip any parameters that have the same pattern as " - "
+            while (true) {
+                if (lastPart == curPart || splitData[lastPart - 1].startsWith("{")) {
+                    break;
+                }
+                lastPart--;
+            }
+            // Get the Query
+            StringBuffer queryStringBuffer = new StringBuffer();
+            for (int i = curPart; i < lastPart - 1; i++) {
+                queryStringBuffer.append(splitData[i]);
+            }
+            record.query = queryStringBuffer.toString().trim();
+
+            return record;
+        }catch (Exception e) {
+            e.printStackTrace();
+            return null ;
+        }
     }
 
 
